c8a21fc8b9ce513234dea4d1fb98d3011d47319a
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-11-19 21:33:57 +0530

    Registration Screen Under Progress

--- app/src/main/java/com/smritiraksha/Registration.java
+++ app/src/main/java/com/smritiraksha/Registration.java
@@ -1,6 +1,10 @@
 package com.smritiraksha;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Registration extends AppCompatActivity {
 
+    TextView log_txt;
+    Button reg_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,16 @@ public class Registration extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        log_txt=findViewById(R.id.login_text);
+        reg_btn=findViewById(R.id.registerButton);
+
+        reg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Registration.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
