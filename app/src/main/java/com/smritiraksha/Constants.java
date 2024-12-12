645ca8bb18bdbc6ee9a2803e605cff331c4015ba
Admin <spoorthi.2jul@gmail.com>
2024-12-12 09:46:32 +0530

    changes made regarding design position.

--- app/src/main/java/com/smritiraksha/Constants.java
+++ app/src/main/java/com/smritiraksha/Constants.java
@@ -5,7 +5,7 @@ import java.net.URL;
 public class Constants {
 
    //root server url
-    private static  String Root_URL = "https://6aa8-182-74-196-22.ngrok-free.app/android/v1/";
+    private static  String Root_URL = " https://b6c9-14-99-164-234.ngrok-free.app/android/v1/";
 
     //registration
     public  static  String URL_REGISTER = Root_URL + "registerUser.php";
