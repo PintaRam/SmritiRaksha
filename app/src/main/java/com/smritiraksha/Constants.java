ff5135680b116091be906de129bd829ad437a16b
Pintaram <pintu.borana20@gmail.com>
2025-02-07 11:38:37 +0530

    Dashboard Update wiht steps calculation

--- app/src/main/java/com/smritiraksha/Constants.java
+++ app/src/main/java/com/smritiraksha/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
    //root server url
 
-    private static  String Root_URL = "https://77fd-2409-408c-9406-c043-2034-58c0-f5b1-75e6.ngrok-free.app/android/v1/";
+    private static  String Root_URL = "https://8279-2409-408c-9406-c043-38b2-5f63-e03d-3e9d.ngrok-free.app/android/v1/";
 
     //registration
     public  static  String URL_REGISTER = Root_URL + "registerUser.php";
